548a951813a53ff99e2a209c2726ed6e7e1200d6
Martin Rossil <martinrossil@gmail.com>
2022-01-04 21:58:37 +0100

    Created Colors.

---
+++ src/theme/Colors.ts
@@ -0,0 +1,9 @@
+import { Color, IColor } from 'enta';
+
+export default class Colors {
+    public static BLUE_DARKEST: IColor = new Color(210, 100, 5);
+    public static BLUE_DARK: IColor = new Color(210, 100, 20);
+    public static BLUE: IColor = new Color(210, 100, 35);
+    public static BLUE_LIGHT: IColor = new Color(210, 100, 50);
+    public static BLUE_LIGHTEST: IColor = new Color(210, 100, 65);
+}
