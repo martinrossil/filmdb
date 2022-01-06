550f66d08b0fa0042c13a85857958cc2d70b9283
Martin Rossil <martinrossil@gmail.com>
2022-01-06 22:33:22 +0100

    Removed dummy data.

--- src/state/Model.ts
+++ src/state/Model.ts
@@ -6,11 +6,7 @@ export default class Model {
     private static _movies: IArrayCollection<IMovie>;
     public static get movies(): IArrayCollection<IMovie> {
         if (!this._movies) {
-            const items: Array<IMovie> = [];
-            for (let i = 0; i < 20; i++) {
-                items.push(new Movie(i + 1, Math.random().toString()))
-            }
-            this._movies = new ArrayCollection(items);
+            this._movies = new ArrayCollection();
         }
         return this._movies;
     }
