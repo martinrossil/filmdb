8d4eb87046ff3fe7f34bc9c269de72cc7e4e3759
Martin Rossil <martinrossil@gmail.com>
2022-01-08 22:39:27 +0100

    Refactored discover function.

--- functions/api/discover.ts
+++ functions/api/discover.ts
@@ -7,17 +7,18 @@ export async function onRequest({request}): Promise<Response> {
     const url = new URL(request.url);
     const search = new URLSearchParams(url.search);
     const [page, error] = await discover(search.toString());
-    let response = '[null, null]';
-    
     if (page) {
         const moviesPage: IMoviesPage = movieDiscoverPageSchemaToMoviesPage(page);
-        response = JSON.stringify(moviesPage, null, 4);
+        return getResponse(JSON.stringify(moviesPage, null, 4));
     }
     if (error) {
-        response = JSON.stringify({ error: Error }, null, 4);
+        return getResponse(JSON.stringify({ error: Error }, null, 4));
     }
-    
-    return new Response(response, {
+    return getResponse(JSON.stringify(new Error('Cloud network error'), null, 4));
+}
+
+function getResponse(body: string): Response {
+    return new Response(body, {
         headers: {
             'content-type': 'application/json;charset=UTF-8',
             'Access-Control-Allow-Origin': '*',
