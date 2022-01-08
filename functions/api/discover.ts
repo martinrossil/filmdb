011defcd3ac0118c9b4c757fb6cb66f4d724c011
Martin Rossil <martinrossil@gmail.com>
2022-01-08 11:45:28 +0100

    Destructed context.

--- functions/api/discover.ts
+++ functions/api/discover.ts
@@ -1,16 +1,7 @@
 /* eslint-disable */
 import { discover } from '../Tmdb';
 
-export async function onRequest(context): Promise<Response> {
-    const {
-        request, // same as existing Worker API
-        env, // same as existing Worker API
-        params, // if filename includes [id] or [[path]]
-        waitUntil, // same as ctx.waitUntil in existing Worker API
-        next, // used for middleware or to fetch assets
-        data // arbitrary space for passing data between middlewares
-      } = context;
-    
+export async function onRequest({request}): Promise<Response> {
     const url = new URL(request.url);
     const search = new URLSearchParams(url.search);
     const [page, error] = await discover(search.toString());
--- functions/api/movie/[id].ts
+++ functions/api/movie/[id].ts
@@ -1,16 +1,7 @@
 /* eslint-disable */
 import { getMovie } from '../../Tmdb';
 
-export async function onRequest(context): Promise<Response> {
-    const {
-        request, // same as existing Worker API
-        env, // same as existing Worker API
-        params, // if filename includes [id] or [[path]]
-        waitUntil, // same as ctx.waitUntil in existing Worker API
-        next, // used for middleware or to fetch assets
-        data // arbitrary space for passing data between middlewares
-    } = context;
-
+export async function onRequest({params}): Promise<Response> {
     const id: number = parseInt(params.id);
     const [movie, error] = await getMovie(id);
     let response = '[null, null]';
