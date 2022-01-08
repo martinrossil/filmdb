105df3feb50cbec4a966dd4f6fbfde15cf5acaef
Martin Rossil <martinrossil@gmail.com>
2022-01-08 12:23:45 +0100

    Added Typography.

---
+++ src/theme/Typography.ts
@@ -0,0 +1,6 @@
+import { ITypeFace, TypeFace } from 'enta';
+
+export default class Typography {
+    private static _sansSerif = 'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"';
+    public static sansSerif: ITypeFace = new TypeFace(Typography._sansSerif, 0.695, 0.09, -0.063);
+}
