2ca6b6328ede487fd087fb8fa6a5c15b14992419
Martin Rossil <martinrossil@gmail.com>
2022-01-08 12:27:45 +0100

    Added 9/16 aspect ratio calculation.

--- src/views/BackdropImage.ts
+++ src/views/BackdropImage.ts
@@ -1,11 +1,20 @@
 import { ImageElement } from 'enta';
+import Colors from '../theme/Colors';
 
 export default class BackdropImage extends ImageElement {
     public constructor() {
         super();
         this.name = 'BackdropImage';
+        this.backgroundColor = Colors.BLUE_DARK;
         this.objectFit = 'cover';
-        this.percentWidth = this.percentHeight = 100;
+        this.percentWidth = 100;
+    }
+
+    protected validate(): void {
+        if (this.actualWidth > 0) {
+            this.internalHeight = this.actualWidth * 0.5625; // 9 / 16
+        }
+        super.validate();
     }
 }
 customElements.define('backdrop-image', BackdropImage);
