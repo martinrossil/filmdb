09ed5f3d80ca47d6164d1fdfc0e884c6f05be38b
Martin Rossil <martinrossil@gmail.com>
2022-01-08 11:19:25 +0100

    Added missing /

--- functions/images/backdrop/[path].ts
+++ functions/images/backdrop/[path].ts
@@ -8,7 +8,7 @@ export async function onRequest(context): Promise<Response> {
         next, // used for middleware or to fetch assets
         data // arbitrary space for passing data between middlewares
     } = context;
-    const URL = 'https://image.tmdb.org/t/p/w300';
+    const URL = 'https://image.tmdb.org/t/p/w300/';
     const path: string = params.path;
     return fetch(URL + path);
 }
