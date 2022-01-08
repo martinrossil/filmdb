2817fd5abcc1d7d05fb9d2254570860b7696d02b
Martin Rossil <martinrossil@gmail.com>
2022-01-08 12:28:20 +0100

    Added title label.

--- src/views/MovieItemRenderer.ts
+++ src/views/MovieItemRenderer.ts
@@ -1,4 +1,5 @@
-import { DataRenderer } from 'enta';
+import { DataRenderer, ILabelElement, VerticalLayout } from 'enta';
+import SerifLabel from '../components/SerifLabel';
 import { MovieDiscoverSchema } from '../schema/MovieDiscoverSchema';
 import Colors from '../theme/Colors';
 import BackdropImage from './BackdropImage';
@@ -6,28 +7,19 @@ import BackdropImage from './BackdropImage';
 export default class MovieItemRenderer extends DataRenderer<MovieDiscoverSchema> {
     public constructor() {
         super();
-        this.backgroundColor = Colors.BLUE_DARK;
-        this.addElement(this.image);
+        this.clip = 'hidden';
+        this.layout = new VerticalLayout(16);
+        this.paddingBottom = 16;
+        this.addElements([this.image, this.titleLabel]);
     }
 
-    /**
-     * backdrop sizes
-     * w300 w780 w1280 original
-     */
     protected dataChanged(): void {
         if (this.data) {
-            this.image.alt = this.data['title'];
+            this.titleLabel.text = this.data.title;
+            this.image.alt = this.data.title;
             if (this.data['backdrop_path']) {
-                this.image.source = 'https://filmdb.pages.dev/images/backdrop' + this.data['backdrop_path'];
+                this.image.source = 'https://filmdb.pages.dev/images/backdrop' + this.data.backdrop_path;
             }
-        }
-    }
-
-    protected updateInternalHeight(): void {
-        if (this.actualWidth > 0) {
-            const height = Math.floor(this.actualWidth * 9 / 16);
-            console.log(this.actualWidth, height);
-            this.internalHeight = height;
         }
     }
 
@@ -37,6 +29,15 @@ export default class MovieItemRenderer extends DataRenderer<MovieDiscoverSchema>
             this._image = new BackdropImage();
         }
         return this._image;
+    }
+
+    private _titleLabel!: ILabelElement;
+    private get titleLabel(): ILabelElement {
+        if (!this._titleLabel) {
+            this._titleLabel = new SerifLabel(16, Colors.BLUE_LIGHTEST);
+            this._titleLabel.percentWidth = 100;
+        }
+        return this._titleLabel;
     }
 }
 customElements.define('movie-item-renderer', MovieItemRenderer);
