efe4ae7e8075cb0e918ad9f846f3409d3acb4c4e
Martin Rossil <martinrossil@gmail.com>
2022-01-08 12:27:13 +0100

    Added reference sizes.

--- functions/images/backdrop/[path].ts
+++ functions/images/backdrop/[path].ts
@@ -1,4 +1,8 @@
 /* eslint-disable */
+/**
+ * backdrop sizes
+ * w300 w780 w1280 original
+ */
 export async function onRequest({params}): Promise<Response> {
     const URL = 'https://image.tmdb.org/t/p/w300/';
     const path: string = params.path;
