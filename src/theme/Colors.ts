131e51f76030297160d98f39fc73549af235d9a9
Martin Rossil <martinrossil@gmail.com>
2022-01-10 19:53:14 +0100

    Added WHITE.

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -1,9 +1,11 @@
 import { Color, IColor } from 'enta';
 
 export default class Colors {
+    public static WHITE: IColor = new Color(0, 0, 100);
+
     public static BLUE_DARKEST: IColor = new Color(210, 100, 5);
-    public static BLUE_DARK: IColor = new Color(210, 100, 20);
-    public static BLUE: IColor = new Color(210, 100, 35);
-    public static BLUE_LIGHT: IColor = new Color(210, 100, 50);
-    public static BLUE_LIGHTEST: IColor = new Color(210, 100, 65);
+    public static BLUE_DARK: IColor = new Color(210, 100, 10);
+    public static BLUE: IColor = new Color(210, 100, 15);
+    public static BLUE_LIGHT: IColor = new Color(210, 100, 80);
+    public static BLUE_LIGHTEST: IColor = new Color(210, 100, 85);
 }
