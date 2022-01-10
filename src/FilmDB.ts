77a404558d858d747cd0c8d3c2e4a2376eb9aba2
Martin Rossil <martinrossil@gmail.com>
2022-01-10 19:55:40 +0100

    Added more UI

--- src/FilmDB.ts
+++ src/FilmDB.ts
@@ -2,14 +2,22 @@ import { ApplicationElement } from 'enta';
 import APIMachine from './machines/APIMachine';
 import NavigationMachine from './machines/NavigationMachine';
 import Colors from './theme/Colors';
+import AppBar from './views/AppBar';
+import GenresNavigation from './views/GenresNavigation';
 import MoviesList from './views/MoviesList';
+import ProvidersNavigation from './views/ProvidersNavigation';
 
 export default class FilmDB extends ApplicationElement {
     public constructor() {
         super();
         this.style.height = '100vh';
         this.backgroundColor = Colors.BLUE_DARKEST;
-        this.addElement(new MoviesList());
+        this.addElements([
+            new AppBar(),
+            new ProvidersNavigation(),
+            new MoviesList(),
+            new GenresNavigation()
+        ]);
     }
 
     private navigationMachine: NavigationMachine = new NavigationMachine(this);
