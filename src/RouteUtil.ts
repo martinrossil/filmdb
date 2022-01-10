c4b48c71661ca5ec11f8ee62f10f4e9ee9157f7c
Martin Rossil <martinrossil@gmail.com>
2022-01-10 19:56:03 +0100

    Created RouteUtil

---
+++ src/RouteUtil.ts
@@ -0,0 +1,11 @@
+export function getAnchorFromEventTarget(target: EventTarget | null): HTMLAnchorElement | null {
+    if (target instanceof HTMLAnchorElement) {
+        return target;
+    }
+    if (target instanceof Document) {
+        return null;
+    }
+    const targetNode: Node = target as Node;
+    const parent: Node | null = targetNode.parentNode;
+    return getAnchorFromEventTarget(parent);
+}
