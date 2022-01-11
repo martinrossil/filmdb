dbba1beb1730d9edd6db6d9d0f55f57979e5cda0
Martin Rossil <martinrossil@gmail.com>
2022-01-11 23:19:17 +0100

    Created Genre vo.

---
+++ functions/vo/Genre.ts
@@ -0,0 +1,10 @@
+export default class Genre {
+    public id: number
+    public label: string;
+    public slug: string;
+    public constructor(label: string, id: number, slug: string) {
+        this.label = label;
+        this.id = id;
+        this.slug = slug;
+    }
+}
