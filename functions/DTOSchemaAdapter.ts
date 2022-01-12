8e9c62293e9afc1ec7aa2308ce64958af4eef662
Martin Rossil <martinrossil@gmail.com>
2022-01-12 19:38:04 +0100

    Removed .jpg from backdrop path.

--- functions/DTOSchemaAdapter.ts
+++ functions/DTOSchemaAdapter.ts
@@ -18,7 +18,7 @@ export function movieDiscoverPageSchemaToMoviesPage(schema: MovieDiscoverPageSch
 
 function movieDiscoverSchemaToMovie(schema: MovieDiscoverSchema): IMovie {
     const movie: IMovie = new Movie();
-    movie.backdrop = schema.backdrop_path;
+    movie.backdrop = schema.backdrop_path.substring(0, schema.backdrop_path.length - 1);
     movie.title = schema.title;
     return movie;
 }
