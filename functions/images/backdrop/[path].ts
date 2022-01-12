8f38e9ea8baaa8c9694cb43a5acbb717aaaf303c
Martin Rossil <martinrossil@gmail.com>
2022-01-12 22:41:41 +0100

    Reverted to w300 sizes.

--- functions/images/backdrop/[path].ts
+++ functions/images/backdrop/[path].ts
@@ -4,11 +4,10 @@
  * w300 w780 w1280 original
  */
 export async function onRequest({request, params}): Promise<Response> {
-    
-    const URL = 'https://imagedelivery.net/7G7XxXgIOc0EMphWhmzfNA/692acda2-236a-4905-b18c-1d13e55ce200/288X192';
-    // const URL = 'https://image.tmdb.org/t/p/w1280/' + params.path + '.jpg';
-    const imageRequest = new Request(URL, {
+    // const URL = 'https://imagedelivery.net/7G7XxXgIOc0EMphWhmzfNA/692acda2-236a-4905-b18c-1d13e55ce200/288X192';
+    const URL = 'https://image.tmdb.org/t/p/w300/' + params.path + '.jpg';
+    /*const imageRequest = new Request(URL, {
         headers: request.headers
-    });
-    return fetch(imageRequest);
+    });*/
+    return fetch(URL);
 }
