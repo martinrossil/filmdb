13bb92aea3bd94f71a50eaa4bd0f4935d6b28263
Martin Rossil <martinrossil@gmail.com>
2022-01-12 19:37:31 +0100

    Testing ondemand resizing.

--- functions/images/backdrop/[path].ts
+++ functions/images/backdrop/[path].ts
@@ -3,8 +3,28 @@
  * backdrop sizes
  * w300 w780 w1280 original
  */
-export async function onRequest({params}): Promise<Response> {
-    const URL = 'https://image.tmdb.org/t/p/w300/';
-    const path: string = params.path;
-    return fetch(URL + path);
+export async function onRequest({request, params}): Promise<Response> {
+    // Cloudflare-specific options are in the cf object.
+    let options = {
+        cf: {
+        image: {
+                width: 288,
+                height: 192,
+                fit: 'cover',
+                metadata: 'none',
+                format: ''
+            } 
+        } 
+    };
+    const accept = request.headers.get("Accept");
+    if (/image\/avif/.test(accept)) {
+        options.cf.image.format = 'avif';
+      } else if (/image\/webp/.test(accept)) {
+        options.cf.image.format = 'webp';
+      }
+
+    const URL = 'https://image.tmdb.org/t/p/w1280/' + params.path + '.jpg';
+    
+    // @ts-ignore
+    return fetch(URL, options);
 }
