b2f58e88f0840a9749d0bbd865ebd4f440575c3e
Martin Rossil <martinrossil@gmail.com>
2022-01-13 20:12:31 +0100

    Cleaned.

--- functions/images/backdrop/[path].ts
+++ functions/images/backdrop/[path].ts
@@ -4,10 +4,6 @@
  * w300 w780 w1280 original
  */
 export async function onRequest({request, params}): Promise<Response> {
-    // const URL = 'https://imagedelivery.net/7G7XxXgIOc0EMphWhmzfNA/692acda2-236a-4905-b18c-1d13e55ce200/288X192';
     const URL = 'https://image.tmdb.org/t/p/w300/' + params.path + '.jpg';
-    /*const imageRequest = new Request(URL, {
-        headers: request.headers
-    });*/
     return fetch(URL);
 }
