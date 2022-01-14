79a190fa5c52d83372ecd75e24147434721f0c5a
Martin Rossil <martinrossil@gmail.com>
2022-01-14 22:16:22 +0100

    Testing without KV store.

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -5,36 +5,36 @@ import { getQueryString } from '../QueryUtil';
 import { discover } from '../Tmdb';
 
 export async function onRequest({ params, env }): Promise<Response> {
-    const TEST = env.TEST;
+    // const TEST = env.TEST;
     if (params.path) {
         const segments: Array<string> = params.path;
         if (segments.length === 4) {
-            const queryString = getQueryString(segments[0], segments[1], segments[2], segments[3]);
-            return getResponse(queryString);
             /*const key: string = segments.join('/');
             const value: string | null = await TEST.get(key);
             if (value !== null) {
                 return getResponse(value);
-            }
+            }*/
             const queryString = getQueryString(segments[0], segments[1], segments[2], segments[3]);
-            return await getDicover(queryString, key, TEST);*/
+            return await getDicover(queryString);
         }
-        return await getDicover('', '/', TEST);
+        return await getDicover('');
+        // return await getDicover('', '/', TEST);
     }
-    const value: string | null = await TEST.get('/');
+    /*const value: string | null = await TEST.get('/');
     if (value !== null) {
         return getResponse(value);
-    }
-    return await getDicover('', '/', TEST);
+    }*/
+    return await getDicover('');
+    // return await getDicover('', '/', TEST);
 }
-
-async function getDicover(query: string, key: string, TEST): Promise<Response> {
+// async function getDicover(query: string, key: string, TEST): Promise<Response> {
+async function getDicover(query: string): Promise<Response> {
     const [page, error] = await discover(query);
         if (page) {
             const moviesPage: IMoviesPage = movieDiscoverPageSchemaToMoviesPage(page);
             const moviesPageString = JSON.stringify(moviesPage, null, 4);
             const ONE_DAY = 60 * 60 * 24;
-            await TEST.put(key, moviesPageString, { expirationTtl: ONE_DAY })
+            // await TEST.put(key, moviesPageString, { expirationTtl: ONE_DAY })
             return getResponse(moviesPageString);
         }
         if (error) {
