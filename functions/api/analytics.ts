88ca5ea70565275280a02f06218fb32404921c33
Martin Rossil <martinrossil@gmail.com>
2022-01-18 10:11:00 +0100

    Testing headers.

--- functions/api/analytics.ts
+++ functions/api/analytics.ts
@@ -2,9 +2,10 @@
 export async function onRequest({request}): Promise<Response> {
     return new Response(JSON.stringify({ 
         headers: {
-            ua: request.headers.get('User-Agent'),
-            cip: request.headers.get('cf-connecting-ip'),
-            ipc: request.headers.get('cf-ipcountry')
+            ua: request.headers.get('user-agent'),
+            cfConnectingIP: request.headers.get('cf-connecting-ip'),
+            cfIPCountry: request.headers.get('cf-ipcountry'),
+            cfRay: request.headers.get('cf-ray')
         },
         cf: request.cf
     }))
