b48b81fa39f0aa45e2ebfe1f98c223725c005dea
Martin Rossil <martinrossil@gmail.com>
2022-01-18 09:39:01 +0100

    Wrapped headers in Map

--- functions/api/analytics.ts
+++ functions/api/analytics.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export async function onRequest({request}): Promise<Response> {
     return new Response(JSON.stringify({ 
-        headers: request.headers,
+        headers: new Map(request.headers),
         cf: request.cf
     }))
 };
