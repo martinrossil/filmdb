e97169dfa775267e7d4acc487c582e39b627d20c
Martin Rossil <martinrossil@gmail.com>
2022-01-18 21:37:17 +0100

    Created backdrop endpoint.

---
+++ functions/images/backdrop/w300/[uid].ts
@@ -0,0 +1,40 @@
+/* eslint-disable */
+import { ImageSchema } from '../../../schema/ImageSchema';
+import { getMovieImages } from '../../../Tmdb';
+
+ export async function onRequest({params, env}): Promise<Response> {
+    const BASE_URL = 'https://image.tmdb.org/t/p/w300/';
+    const uid: string = params.uid;
+    const BACKDROP_PATHS = env.BACKDROP_PATHS;
+    const path: string | null = await BACKDROP_PATHS.get(uid);
+    if (path) {
+        const URL = BASE_URL + path;
+        return fetch(URL);
+    }
+    const id = parseInt(uid, 16);
+    const [movieImages, error] = await getMovieImages(id);
+    if (movieImages) {
+        if (movieImages.backdrops.length) {
+            const backdrop: ImageSchema = movieImages.backdrops[0];
+            const path = backdrop.file_path;
+            const ONE_DAY = 60 * 60 * 24;
+            await BACKDROP_PATHS.put(uid, path, { expirationTtl: ONE_DAY })
+            const URL = BASE_URL + path;
+            return fetch(URL);
+        }
+    }
+    if (error) {
+        return getResponse(JSON.stringify({ error: Error }, null, 4));
+    }
+    return getResponse(JSON.stringify(new Error('Cloud network error'), null, 4));
+}
+
+function getResponse(body: string): Response {
+    return new Response(body, {
+        headers: {
+            'content-type': 'application/json;charset=UTF-8',
+            'Cache-Control': 'public, max-age=86400',
+            'Access-Control-Allow-Origin': '*'
+        }
+    });
+}
