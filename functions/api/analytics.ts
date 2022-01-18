df3259a558a7c1c34e1e4051dfab03e4530b82f6
Martin Rossil <martinrossil@gmail.com>
2022-01-18 20:36:36 +0100

    Cleaned.

--- functions/api/analytics.ts
+++ functions/api/analytics.ts
@@ -4,14 +4,9 @@ export async function onRequest({request}): Promise<Response> {
     request.headers.forEach((value, key) => {
         headers.push({ key, value });
     });
+    const { cf } = request;
     return new Response(JSON.stringify({ 
         headers,
-        /*headers: {
-            ua: request.headers.get('user-agent'),
-            cfConnectingIP: request.headers.get('cf-connecting-ip'),
-            cfIPCountry: request.headers.get('cf-ipcountry'),
-            cfRay: request.headers.get('cf-ray')
-        },*/
-        cf: request.cf
+        cf
     }, null, 4))
 };
