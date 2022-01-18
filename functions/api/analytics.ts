df3ff79288642fe55b5236f39b80323e6f2a35af
Martin Rossil <martinrossil@gmail.com>
2022-01-18 09:47:18 +0100

    Testing headers.

--- functions/api/analytics.ts
+++ functions/api/analytics.ts
@@ -1,7 +1,11 @@
 /* eslint-disable */
 export async function onRequest({request}): Promise<Response> {
     return new Response(JSON.stringify({ 
-        headers: new Map(request.headers),
+        headers: {
+            ua: request.headers.get('User-Agent'),
+            cip: request.headers.get('cf-connecting-ip'),
+            ipc: request.headers.get('cf-ipcountry')
+        },
         cf: request.cf
     }))
 };
