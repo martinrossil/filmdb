bd05c18602e4596822bc51ec8850b8bbcfad1efb
Martin Rossil <martinrossil@gmail.com>
2022-02-05 09:58:29 +0100

    Fixes #1

--- src/FilmDB.ts
+++ src/FilmDB.ts
@@ -1,29 +1,13 @@
-import { ApplicationElement, IMachine } from 'enta';
-import APIMachine from './machines/APIMachine';
-import FilmDBMachine from './machines/FilmDBMachine';
-import NavigationMachine from './machines/NavigationMachine';
+import { ApplicationElement } from 'enta';
 import Colors from './theme/Colors';
-import AppBar from './views/AppBar';
-import GenresNavigation from './views/GenresNavigation';
-import MoviesList from './views/MoviesList';
-import ProvidersNavigation from './views/ProvidersNavigation';
+import ResponsiveNavigator from './views/ResponsiveNavigator';
 
 export default class FilmDB extends ApplicationElement {
     public constructor() {
         super();
         this.style.height = '100vh';
         document.body.style.backgroundColor = Colors.BLUE_DARKEST.toString();
-        /* this.addElements([
-            new AppBar(),
-            new ProvidersNavigation(),
-            new MoviesList(),
-            new GenresNavigation()
-        ]); */
+        this.addElement(new ResponsiveNavigator());
     }
-
-    private filmDBMachine: IMachine = new FilmDBMachine(this);
-
-    // private navigationMachine: NavigationMachine = new NavigationMachine(this);
-    // private apiMachine: APIMachine = new APIMachine(this);
 }
 customElements.define('film-db', FilmDB);
