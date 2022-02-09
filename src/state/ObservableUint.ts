537f017f0097b46e9651ccc0b9cb5221ee2b2bbe
Martin Rossil <martinrossil@gmail.com>
2022-02-09 15:38:25 +0100

    Created ObservableUint.

---
+++ src/state/ObservableUint.ts
@@ -0,0 +1,41 @@
+import { EventDispatcher } from 'enta';
+
+export default class OberservableUint extends EventDispatcher {
+    public constructor(value = 0) {
+        super();
+        this.name = 'OberservableUint';
+        this._value = value;
+    }
+
+    private _value;
+
+    public set value(value: number) {
+        if (isNaN(value)) {
+            if (this._value !== 0) {
+                this._value = 0;
+                this.notify();
+            }
+            return;
+        }
+        if (value < 0) {
+            if (this._value !== 0) {
+                this._value = 0;
+                this.notify();
+            }
+            return;
+        }
+        const uint = Math.round(value);
+        if (this._value !== uint) {
+            this._value = uint;
+            this.notify();
+        }
+    }
+
+    public get value(): number {
+        return this._value;
+    }
+
+    private notify(): void {
+        this.dispatchEvent(new CustomEvent('changed', { detail: this.value }));
+    }
+}
