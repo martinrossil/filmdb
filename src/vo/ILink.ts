d9a6c36965decebaaac2aa897aa149d169360116
Martin Rossil <martinrossil@gmail.com>
2022-02-09 15:37:22 +0100

    Added defaultHref property.

--- src/vo/ILink.ts
+++ src/vo/ILink.ts
@@ -1,6 +1,7 @@
 import { IEventDispatcher } from 'enta';
 
 export default interface ILink extends IEventDispatcher {
+    defaultHref: string;
     href: string;
     label: string;
     selected: boolean;
