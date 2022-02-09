f2a56a4fd90d4818597408af568f21a660153762
Martin Rossil <martinrossil@gmail.com>
2022-02-09 15:38:43 +0100

    Added 2 colors.

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -8,4 +8,7 @@ export default class Colors {
     public static BLUE: IColor = new Color(210, 100, 15);
     public static BLUE_LIGHT: IColor = new Color(210, 100, 80);
     public static BLUE_LIGHTEST: IColor = new Color(210, 100, 85);
+
+    public static STAR_BLUE: IColor = new Color(210, 67, 48);
+    public static STAR_ORANGE: IColor = new Color(40, 67, 48);
 }
