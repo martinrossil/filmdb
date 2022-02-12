3a9182af2e1f05e1fdf98775b9caeb7cb5e68ea6
Martin Rossil <martinrossil@gmail.com>
2022-02-12 16:54:11 +0100

    Added routing.

--- src/machines/FilmDBMachines.ts
+++ src/machines/FilmDBMachines.ts
@@ -2,15 +2,18 @@ import FilmDB from '../FilmDB';
 import LinkMachine from './LinkMachine';
 import NavigationMachine from './NavigationMachine';
 import ResponsiveMachine from './ResponsiveMachine';
+import RoutingMachine from './RoutingMachine';
 
 export default class FilmDBMachines {
     public constructor(filmDB: FilmDB) {
         this.responsiveMachine = new ResponsiveMachine(filmDB);
         this.navigationMachine = new NavigationMachine(filmDB);
+        this.routingMachine = new RoutingMachine(filmDB);
         this.linkMachine = new LinkMachine(filmDB);
     }
 
     private responsiveMachine: ResponsiveMachine;
     private navigationMachine: NavigationMachine;
+    private routingMachine: RoutingMachine;
     private linkMachine: LinkMachine;
 }
