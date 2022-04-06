767b7635ede05fc20a8e64057a3978362c539a59
Martin Rossil <martinrossil@gmail.com>
2022-04-06 19:36:28 +0200

    WIP

--- src/FilmDB.ts
+++ src/FilmDB.ts
@@ -1,11 +1,9 @@
 import { Application } from 'fuix';
 import ResponsiveMachine from './machines/ResponsiveMachine';
-import Colors from './theme/Colors';
+
 export default class FilmDB extends Application {
     public constructor() {
         super();
-        this.style.height = '100vh';
-        this.bodyBackgroundColor = Colors.BLUE_DARKEST;
         new ResponsiveMachine(this);
     }
 }
