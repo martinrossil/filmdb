aa0599f2a759aa3bb421adf4608332fac0682d5c
Martin Rossil <martinrossil@gmail.com>
2022-04-07 12:54:16 +0200

    Moved files to parking repo.

--- src/theme/Typography.ts
+++
@@ -1,4 +0,0 @@
-export default class Typography {
-    private static _sansSerif = 'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"';
-    // public static sansSerif: ITypeFace = new TypeFace(Typography._sansSerif, 0.695, 0.001, -0.063);
-}
