242b58b14800f10b881ff6c47d1cecc3a13c7204
Martin Rossil <martinrossil@gmail.com>
2022-04-07 12:53:21 +0200

    Moved files to parking repo.

--- functions/schema/MovieDiscoverPageSchema.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable camelcase */
-import { MovieDiscoverSchema } from './MovieDiscoverSchema';
-
-export type MovieDiscoverPageSchema = {
-    page: number,
-    results: Array<MovieDiscoverSchema>,
-    total_results: number,
-    total_pages: number
-}
