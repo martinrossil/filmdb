3d94811db7febe5f2bea32098aac444bf83f5993
Martin Rossil <martinrossil@gmail.com>
2022-04-07 12:53:04 +0200

    Moved files to parking repo.

--- functions/images/backdrop/[path].ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-/**
- * backdrop sizes
- * w300 w780 w1280 original
- */
-export async function onRequest({request, params}): Promise<Response> {
-    const URL = 'https://image.tmdb.org/t/p/w300/' + params.path + '.jpg';
-    return fetch(URL);
-}
