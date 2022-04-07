5548afef649506bec1f2a9aa1a052b7018a7b0b2
Martin Rossil <martinrossil@gmail.com>
2022-04-07 12:54:34 +0200

    Moved files to parking repo.

--- src/vo/ILink.ts
+++
@@ -1,8 +0,0 @@
-import { IEventDispatcher } from 'fuix';
-
-export default interface ILink extends IEventDispatcher {
-    href: string;
-    label: string;
-    selected: boolean;
-    slug: string;
-}
