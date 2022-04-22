1590b1ee4758e8d97431d7f49996aa7fb1e7cc8d
Martin Rossil <martinrossil@gmail.com>
2022-04-22 20:20:30 +0200

    Testing fuix dep.

--- src/FilmDB.ts
+++ src/FilmDB.ts
@@ -1,9 +1,20 @@
-import { Application } from 'fuix';
+import { Application, Display } from 'fuix';
+import IEventListener from 'fuix/lib/event/IEventListener';
+import TestComponent from './test/TestComponent';
+import TestComponentEvent from './test/TestComponentEvent';
 
 export default class FilmDB extends Application {
     public constructor() {
         super();
         console.log('FilmDB()');
+        this.display = Display.FLEX;
+        this.style.flexDirection = 'column';
+        this.addComponents([new TestComponent(), new TestComponent(), new TestComponent(), new TestComponent()]);
+        this.addEventListener(TestComponentEvent.TEST, this.testClicked as IEventListener);
+    }
+
+    private testClicked(e: TestComponentEvent): void {
+        console.log(this, e.type, e.prop);
     }
 }
 customElements.define('film-db', FilmDB);
