5911e22ae5e41487ddf7ea2720082cad1fd70a0d
Martin Rossil <martinrossil@gmail.com>
2022-04-29 11:03:53 +0200

    Cleaned.

--- src/FilmDB.ts
+++ src/FilmDB.ts
@@ -1,20 +1,8 @@
-import { Application, Display } from 'fuix';
-import IEventListener from 'fuix/lib/event/IEventListener';
-import TestComponent from './test/TestComponent';
-import TestComponentEvent from './test/TestComponentEvent';
+import { Application } from 'fuix';
 
 export default class FilmDB extends Application {
     public constructor() {
         super();
-        console.log('FilmDB()');
-        this.display = Display.FLEX;
-        this.style.flexDirection = 'column';
-        this.addComponents([new TestComponent(), new TestComponent(), new TestComponent(), new TestComponent()]);
-        this.addEventListener(TestComponentEvent.TEST, this.testClicked as IEventListener);
-    }
-
-    private testClicked(e: TestComponentEvent): void {
-        console.log(this, e.type, e.prop);
     }
 }
 customElements.define('film-db', FilmDB);
