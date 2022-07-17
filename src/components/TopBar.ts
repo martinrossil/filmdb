58ec8694b205bc2a856828a408abeb5aab240ae3
Martin Rossil <martinrossil@gmail.com>
2022-07-17 12:16:50 +0200

    Fixes #8

---
+++ src/components/TopBar.ts
@@ -0,0 +1,8 @@
+import { Container } from 'fuix';
+
+export default class TopBar extends Container {
+    public constructor() {
+        super();
+    }
+}
+customElements.define('top-bar', TopBar);
