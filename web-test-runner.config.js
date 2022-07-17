bf5b7f94b05ea6a722e7cfccd6349d2ce8b854cc
Martin Rossil <martinrossil@gmail.com>
2022-07-17 15:13:56 +0200

    Added reporters.

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,8 +1,16 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { defaultReporter, summaryReporter } from '@web/test-runner';
 
 export default {
     files: ['src/**/*.test.ts'],
     plugins: [esbuildPlugin({ ts: true })],
+    reporters: [
+        summaryReporter(),
+        defaultReporter({
+            reportTestResults: true,
+            reportTestProgress: true
+        })
+    ],
     coverageConfig: {
         report: true,
         reportDir: 'coverage',
