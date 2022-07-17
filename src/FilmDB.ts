a4701c0627a115e96caebc295d8571aab286364f
Martin Rossil <martinrossil@gmail.com>
2022-07-17 12:33:22 +0200

    Fixes #10

--- src/FilmDB.ts
+++ src/FilmDB.ts
@@ -1,8 +1,12 @@
 import { Application } from 'fuix';
+import BottomNavigation from './components/BottomNavigation';
+import TopBar from './components/TopBar';
 
 export default class FilmDB extends Application {
     public constructor() {
         super();
+        this.bodyBackgroundColor = '#F4F5F7';
+        this.addComponents([new TopBar(), new BottomNavigation()]);
     }
 }
 customElements.define('film-db', FilmDB);
--- src/components/BottomNavigation.ts
+++ src/components/BottomNavigation.ts
@@ -1,14 +1,15 @@
 import { Container } from 'fuix';
 
-export default class TopBar extends Container {
+export default class BottomNavigation extends Container {
     public constructor() {
         super();
         this.backgroundColor = 'white';
         this.position = 'fixed';
         this.display = 'block';
         this.height = 64;
+        this.bottom = 0;
         this.left = 0;
         this.right = 0;
     }
 }
-customElements.define('top-bar', TopBar);
+customElements.define('bottom-navigation', BottomNavigation);
