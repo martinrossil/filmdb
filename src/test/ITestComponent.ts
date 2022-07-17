a800d6cab0c646a23851563a40c3401ca30c33e5
Martin Rossil <martinrossil@gmail.com>
2022-07-17 11:17:21 +0200

    Deleted test components.

--- src/test/ITestComponent.ts
+++
@@ -1,7 +0,0 @@
-import { IComponent } from 'fuix';
-
-export default interface ITestComponent extends IComponent {
-    test(input: string): string;
-    twenty: number;
-    nums: 'one' | 'two';
-}
