b94a56f9da37f3f5037be4bc2d87320debba374f
Martin Rossil <martinrossil@gmail.com>
2022-07-18 14:47:05 +0200

    Fixes #18 #19

--- src/FilmDB.ts
+++ src/FilmDB.ts
@@ -1,4 +1,4 @@
-import { Application } from 'fuix';
+import { Application, IParagraph, Paragraph } from 'fuix';
 import BottomNavigation from './components/BottomNavigation';
 import IBottomNavigation from './components/IBottomNavigation';
 import ITopBar from './components/ITopBar';
@@ -10,7 +10,34 @@ export default class FilmDB extends Application implements IFilmDB {
         super();
         this.bodyBackgroundColor = '#F4F5F7';
         this.bodyFontFamily = 'Inter';
-        this.addComponents([this.topBar, this.bottomNavigation]);
+        this.addComponents([this.topBar, this.bottomNavigation, this.interParagraph, this.eurostileParagraph]);
+    }
+
+    #interParagraph!: IParagraph;
+
+    public get interParagraph(): IParagraph {
+        if (!this.#interParagraph) {
+            this.#interParagraph = new Paragraph();
+            this.#interParagraph.text = 'Hæøå ÆØÅ H';
+            this.#interParagraph.fontSize = 64;
+            this.#interParagraph.fontWeight = 700;
+            this.#interParagraph.lineHeight = 0.72;
+        }
+        return this.#interParagraph;
+    }
+
+    #eurostileParagraph!: IParagraph;
+
+    public get eurostileParagraph(): IParagraph {
+        if (!this.#eurostileParagraph) {
+            this.#eurostileParagraph = new Paragraph();
+            this.#eurostileParagraph.text = 'Hæøå ÆØÅ H';
+            this.#eurostileParagraph.fontFamily = 'Eurostile Extd';
+            this.#eurostileParagraph.fontSize = 64;
+            this.#eurostileParagraph.fontWeight = 700;
+            this.#eurostileParagraph.lineHeight = 0.68;
+        }
+        return this.#eurostileParagraph;
     }
 
     #topBar: ITopBar = new TopBar();
