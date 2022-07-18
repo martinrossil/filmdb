0b158055aee5cbddacf520e208c74df744f052cb
Martin Rossil <martinrossil@gmail.com>
2022-07-18 19:25:13 +0200

    Testing ObservableRoute.

--- src/FilmDB.ts
+++ src/FilmDB.ts
@@ -1,16 +1,42 @@
-import { Application, IParagraph, Paragraph } from 'fuix';
+import { Application, IComponent, IParagraph, Paragraph } from 'fuix';
 import BottomNavigation from './components/BottomNavigation';
 import IBottomNavigation from './components/IBottomNavigation';
 import ITopBar from './components/ITopBar';
 import TopBar from './components/TopBar';
 import IFilmDB from './IFilmDB';
+import ObservableRoute from './observables/ObservableRoute';
 
 export default class FilmDB extends Application implements IFilmDB {
     public constructor() {
         super();
+        console.log('FilmDB constructor');
         this.bodyBackgroundColor = '#F4F5F7';
         this.bodyFontFamily = 'Inter';
-        this.addComponents([this.topBar, this.bottomNavigation, this.interParagraph, this.eurostileParagraph]);
+        this.addComponents([
+            this.topBar,
+            this.bottomNavigation,
+            this.interParagraph,
+            this.eurostileParagraph,
+            this.moviesAnchor as unknown as IComponent,
+        ]);
+        console.log(window.location.search, window.location.hash);
+        console.log('route', ObservableRoute.singleton.route);
+        ObservableRoute.singleton.addEventListener(ObservableRoute.ROUTE_CHANGED, this.#routeChanged.bind(this) as EventListener);
+    }
+
+    #moviesAnchor!: HTMLAnchorElement;
+
+    protected get moviesAnchor(): HTMLAnchorElement {
+        if (!this.#moviesAnchor) {
+            this.#moviesAnchor = document.createElement('a');
+            this.#moviesAnchor.innerText = 'Movies';
+            this.#moviesAnchor.href = '/movies';
+        }
+        return this.#moviesAnchor;
+    }
+
+    #routeChanged(e: CustomEvent<string>): void {
+        console.log(e.type, e.detail);
     }
 
     #interParagraph!: IParagraph;
