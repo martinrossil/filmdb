4e7d1ada822d6f0ae48a6a4737d1b416104d13c7
Martin Rossil <martinrossil@gmail.com>
2022-07-19 21:19:29 +0200

    #Fixes 20

--- src/FilmDB.ts
+++ src/FilmDB.ts
@@ -1,4 +1,4 @@
-import { Application, IComponent, IParagraph, Paragraph } from 'fuix';
+import { Application, ILinkContainer, IParagraph, LinkContainer, Paragraph } from 'fuix';
 import BottomNavigation from './components/BottomNavigation';
 import IBottomNavigation from './components/IBottomNavigation';
 import ITopBar from './components/ITopBar';
@@ -9,7 +9,6 @@ import ObservableRoute from './observables/ObservableRoute';
 export default class FilmDB extends Application implements IFilmDB {
     public constructor() {
         super();
-        console.log('FilmDB constructor');
         this.bodyBackgroundColor = '#F4F5F7';
         this.bodyFontFamily = 'Inter';
         this.addComponents([
@@ -17,11 +16,23 @@ export default class FilmDB extends Application implements IFilmDB {
             this.bottomNavigation,
             this.interParagraph,
             this.eurostileParagraph,
-            this.moviesAnchor as unknown as IComponent,
+            this.linkContainer,
         ]);
-        console.log(window.location.search, window.location.hash);
-        console.log('route', ObservableRoute.singleton.route);
         ObservableRoute.singleton.addEventListener(ObservableRoute.ROUTE_CHANGED, this.#routeChanged.bind(this) as EventListener);
+    }
+
+    #linkContainer!: ILinkContainer;
+
+    protected get linkContainer(): ILinkContainer {
+        if (!this.#linkContainer) {
+            this.#linkContainer = new LinkContainer();
+            this.#linkContainer.display = 'inline-block';
+            this.#linkContainer.width = 200;
+            this.#linkContainer.height = 200;
+            this.#linkContainer.backgroundColor = 'white';
+            this.#linkContainer.href = '/film';
+        }
+        return this.#linkContainer;
     }
 
     #moviesAnchor!: HTMLAnchorElement;
@@ -30,7 +41,7 @@ export default class FilmDB extends Application implements IFilmDB {
         if (!this.#moviesAnchor) {
             this.#moviesAnchor = document.createElement('a');
             this.#moviesAnchor.innerText = 'Movies';
-            this.#moviesAnchor.href = '/movies';
+            // this.#moviesAnchor.href = '/movies';
         }
         return this.#moviesAnchor;
     }
