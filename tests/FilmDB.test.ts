7b9601e174fd3a1f41ef7e251f0a687a649c3e48
Martin Rossil <martinrossil@gmail.com>
2022-07-20 23:32:35 +0200

    Fixed broken test.

--- tests/FilmDB.test.ts
+++ tests/FilmDB.test.ts
@@ -3,11 +3,11 @@ import FilmDB from '../src/FilmDB';
 
 describe('FilmDB', () => {
     describe('default body styles', () => {
-        it('computed body backgroundColor should be "rgb(244, 245, 247)"', () => {
+        it('computed body backgroundColor should be "rgb(16, 19, 35)"', () => {
             const filmDB: FilmDB = new FilmDB();
             document.body.appendChild(filmDB);
             const computed = window.getComputedStyle(document.body);
-            assert.strictEqual(computed.backgroundColor, 'rgb(244, 245, 247)');
+            assert.strictEqual(computed.backgroundColor, 'rgb(16, 19, 35)');
             document.body.removeChild(filmDB);
         });
         it('computed body fontFamily should be "Inter"', () => {
