d7cf9ff74e776ebaa05c70600a237d9fb6221b41
Martin Rossil <martinrossil@gmail.com>
2022-07-20 23:28:43 +0200

    Testing Topbar.

--- src/FilmDB.ts
+++ src/FilmDB.ts
@@ -1,11 +1,14 @@
 import { Application } from 'fuix';
+import TopBar from './components/TopBar';
 import IFilmDB from './IFilmDB';
+import Theme from './theme/Theme';
 
 export default class FilmDB extends Application implements IFilmDB {
     public constructor() {
         super();
-        this.bodyBackgroundColor = '#F4F5F7';
+        this.bodyBackgroundColor = Theme.singleton.colors.neutral.COLOR_900;
         this.bodyFontFamily = 'Inter';
+        this.addComponents([new TopBar()]);
     }
 }
 customElements.define('film-db', FilmDB);
--- src/components/TopBar.ts
+++ src/components/TopBar.ts
@@ -4,12 +4,15 @@ import ITopBar from './ITopBar';
 export default class TopBar extends Container implements ITopBar {
     public constructor() {
         super();
-        this.backgroundColor = 'white';
+        this.backgroundColor = 'rgba(20, 36, 51, 0.8)';
         this.position = 'fixed';
         this.display = 'block';
         this.height = 64;
         this.left = 0;
         this.right = 0;
+        this.style.borderBottom = '2px solid #22476b';
+        // this.style.boxShadow = 'inset 0 0 20px rgb(102 179 255 / 20%)';
+        this.style.backgroundImage = 'linear-gradient(0deg, rgba(20, 61, 102, 0.2), rgba(20, 61, 102, 0) 50%)';
     }
 }
 customElements.define('top-bar', TopBar);
