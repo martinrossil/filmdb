df0c8dd2c3f2d8069af3998b834828d2f35ee0c2
Martin Rossil <martinrossil@gmail.com>
2022-07-21 15:30:23 +0200

    Testing Device.

--- src/FilmDB.ts
+++ src/FilmDB.ts
@@ -1,6 +1,7 @@
 import { Application } from 'fuix';
 import TopBar from './components/TopBar';
 import IFilmDB from './IFilmDB';
+import Device from './observables/Device';
 import Theme from './theme/Theme';
 
 export default class FilmDB extends Application implements IFilmDB {
@@ -9,6 +10,12 @@ export default class FilmDB extends Application implements IFilmDB {
         this.bodyBackgroundColor = Theme.singleton.colors.neutral.COLOR_900;
         this.bodyFontFamily = 'Inter';
         this.addComponents([new TopBar()]);
+        Device.singleton.addEventListener(Device.CHANGED, this.#deviceChanged.bind(this));
+        this.#deviceChanged();
+    }
+
+    #deviceChanged(): void {
+        console.log(Device.singleton);
     }
 }
 customElements.define('film-db', FilmDB);
