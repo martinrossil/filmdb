8986b29146d717527c974a53e2eca22a0c84d845
Martin Rossil <martinrossil@gmail.com>
2022-07-22 22:30:31 +0200

    Created MobileMoviesList.

---
+++ src/apps/mobile/IMobileMoviesList.ts
@@ -0,0 +1,6 @@
+import { IDataContainer } from 'fuix';
+import IMoviePreview from '../domain/IMoviePreview';
+
+export default interface IMobileMoviesList extends IDataContainer<IMoviePreview> {
+
+}
