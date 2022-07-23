d9bbb9f08db3e91a1efd240dbb6b697718899c8d
Martin Rossil <martinrossil@gmail.com>
2022-07-23 17:20:38 +0200

    Added child components.

--- src/apps/tablet/TabletLandscapeApp.ts
+++ src/apps/tablet/TabletLandscapeApp.ts
@@ -1,8 +1,64 @@
-import { Container } from 'fuix';
+import { Container, DataContainer, IContainer } from 'fuix';
+import Model from '../../observables/Model';
+import IColors from '../../theme/IColors';
+import Theme from '../../theme/Theme';
+import IMoviePreview from '../../domain/IMoviePreview';
+import MovieRenderer from '../mobile/MovieRenderer';
 
 export default class TabletLandscapeApp extends Container {
     public constructor() {
         super();
+        this.addComponents([this.moviesList, this.navigation, this.topBar]);
+    }
+
+    #colors: IColors = Theme.singleton.colors;
+
+    #moviesList!: DataContainer<IMoviePreview>;
+
+    private get moviesList(): DataContainer<IMoviePreview> {
+        if (!this.#moviesList) {
+            this.#moviesList = new DataContainer();
+            this.#moviesList.display = 'grid';
+            this.#moviesList.style['gap'] = '16px';
+            this.#moviesList.padding = 16;
+            this.#moviesList.style.paddingLeft = '96px';
+            this.#moviesList.style.paddingTop = '88px';
+            this.#moviesList.style.paddingBottom = '16px';
+            this.#moviesList.style['gridTemplateColumns'] = 'repeat(auto-fill, minmax(150px, 1fr))';
+            this.#moviesList.DataRendererClass = MovieRenderer;
+            this.#moviesList.dataProvider = Model.singleton.movies;
+        }
+        return this.#moviesList;
+    }
+
+    #navigation!: IContainer;
+
+    private get navigation(): IContainer {
+        if (!this.#navigation) {
+            this.#navigation = new Container();
+            this.#navigation.backgroundColor = this.#colors.primary.COLOR_700;
+            this.#navigation.position = 'fixed';
+            this.#navigation.width = 80;
+            this.#navigation.left = 0;
+            this.#navigation.bottom = 0;
+            this.#navigation.top = 72;
+        }
+        return this.#navigation;
+    }
+
+    #topBar!: IContainer;
+
+    public get topBar(): IContainer {
+        if (!this.#topBar) {
+            this.#topBar = new Container();
+            this.#topBar.backgroundColor = this.#colors.primary.COLOR_900;
+            this.#topBar.position = 'fixed';
+            this.#topBar.height = 72;
+            this.#topBar.top = 0;
+            this.#topBar.left = 0;
+            this.#topBar.right = 0;
+        }
+        return this.#topBar;
     }
 }
 customElements.define('tablet-landscape-app', TabletLandscapeApp);
