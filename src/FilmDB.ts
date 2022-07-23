ba2994860788d63dd27b3cef2fbd02d43492630e
Martin Rossil <martinrossil@gmail.com>
2022-07-23 17:18:11 +0200

    Fixes #32

--- src/FilmDB.ts
+++ src/FilmDB.ts
@@ -1,24 +1,103 @@
-import { Application } from 'fuix';
-import AppsContainer from './apps/AppsContainer';
-import IAppsContainer from './apps/IAppsContainer';
-import IFilmDB from './IFilmDB';
+import { Application, IComponent } from 'fuix';
+import DesktopApp from './apps/desktop/DesktopApp';
+import LaptopApp from './apps/laptop/LaptopApp';
+import MobileApp from './apps/mobile/MobileApp';
+import TabletLandscapeApp from './apps/tablet/TabletLandscapeApp';
+import TabletPortraitApp from './apps/tablet/TabletPortraitApp';
+import Device from './observables/Device';
+import IDevice from './observables/IDevice';
+import IColors from './theme/IColors';
 import Theme from './theme/Theme';
 
-export default class FilmDB extends Application implements IFilmDB {
+export default class FilmDB extends Application {
     public constructor() {
         super();
-        this.bodyBackgroundColor = Theme.singleton.colors.neutral.COLOR_900;
+        this.display = 'block';
         this.bodyFontFamily = 'Inter';
-        this.addComponent(this.appsContainer);
+        this.bodyBackgroundColor = this.#colors.neutral.COLOR_900;
+        this.#device.addEventListener(Device.CHANGED, this.#deviceChanged.bind(this));
+        this.#deviceChanged();
     }
 
-    #appsContainer!: IAppsContainer;
+    #deviceChanged(): void {
+        this.removeAllComponents();
+        this.addComponent(this.#getAppFromDeviceProperties());
+    }
 
-    public get appsContainer(): IAppsContainer {
-        if (!this.#appsContainer) {
-            this.#appsContainer = new AppsContainer();
+    #getAppFromDeviceProperties(): IComponent {
+        const { name, orientation } = this.#device;
+        if (name === 'mobile') {
+            return this.mobileApp;
         }
-        return this.#appsContainer;
+        if (name === 'tablet') {
+            if (orientation === 'portrait') {
+                return this.tabletPortraitApp;
+            }
+            return this.tabletLandscapeApp;
+        }
+        if (name === 'laptop') {
+            return this.laptopApp;
+        }
+        return this.desktopApp;
+    }
+
+    #colors: IColors = Theme.singleton.colors;
+
+    #device: IDevice = Device.singleton;
+
+    #mobileApp!: MobileApp;
+
+    public get mobileApp(): MobileApp {
+        if (!this.#mobileApp) {
+            this.#mobileApp = new MobileApp();
+            this.#mobileApp.position = 'absolute';
+            this.#mobileApp.inset = '0';
+        }
+        return this.#mobileApp;
+    }
+
+    #tabletPortraitApp!: TabletPortraitApp;
+
+    public get tabletPortraitApp(): TabletPortraitApp {
+        if (!this.#tabletPortraitApp) {
+            this.#tabletPortraitApp = new TabletPortraitApp();
+            this.#tabletPortraitApp.position = 'absolute';
+            this.#tabletPortraitApp.inset = '0';
+        }
+        return this.#tabletPortraitApp;
+    }
+
+    #tabletLandscapeApp!: TabletLandscapeApp;
+
+    public get tabletLandscapeApp(): TabletLandscapeApp {
+        if (!this.#tabletLandscapeApp) {
+            this.#tabletLandscapeApp = new TabletLandscapeApp();
+            this.#tabletLandscapeApp.position = 'absolute';
+            this.#tabletLandscapeApp.inset = '0';
+        }
+        return this.#tabletLandscapeApp;
+    }
+
+    #laptopApp!: LaptopApp;
+
+    public get laptopApp(): LaptopApp {
+        if (!this.#laptopApp) {
+            this.#laptopApp = new LaptopApp();
+            this.#laptopApp.position = 'absolute';
+            this.#laptopApp.inset = '0';
+        }
+        return this.#laptopApp;
+    }
+
+    #desktopApp!: DesktopApp;
+
+    public get desktopApp(): DesktopApp {
+        if (!this.#desktopApp) {
+            this.#desktopApp = new DesktopApp();
+            this.#desktopApp.position = 'absolute';
+            this.#desktopApp.inset = '0';
+        }
+        return this.#desktopApp;
     }
 }
 customElements.define('film-db', FilmDB);
