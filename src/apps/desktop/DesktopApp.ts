fa48647fc2f1b5a39c4aa3cfdc177f8c946135d3
Martin Rossil <martinrossil@gmail.com>
2022-07-23 17:19:44 +0200

    Added child components.

--- src/apps/desktop/DesktopApp.ts
+++ src/apps/desktop/DesktopApp.ts
@@ -1,8 +1,63 @@
-import { Container } from 'fuix';
+import { Container, DataContainer, IContainer } from 'fuix';
+import Model from '../../observables/Model';
+import IColors from '../../theme/IColors';
+import Theme from '../../theme/Theme';
+import IMoviePreview from '../../domain/IMoviePreview';
+import MovieRenderer from '../mobile/MovieRenderer';
 
 export default class DesktopApp extends Container {
     public constructor() {
         super();
+        this.addComponents([this.moviesList, this.navigation, this.topBar]);
+    }
+
+    #colors: IColors = Theme.singleton.colors;
+
+    #moviesList!: DataContainer<IMoviePreview>;
+
+    private get moviesList(): DataContainer<IMoviePreview> {
+        if (!this.#moviesList) {
+            this.#moviesList = new DataContainer();
+            this.#moviesList.display = 'grid';
+            this.#moviesList.style['gap'] = '24px';
+            this.#moviesList.padding = 24;
+            this.#moviesList.style.paddingTop = '152px';
+            this.#moviesList.style.paddingBottom = '24px';
+            this.#moviesList.style['gridTemplateColumns'] = 'repeat(auto-fill, minmax(200px, 1fr))';
+            this.#moviesList.DataRendererClass = MovieRenderer;
+            this.#moviesList.dataProvider = Model.singleton.movies;
+        }
+        return this.#moviesList;
+    }
+
+    #navigation!: IContainer;
+
+    private get navigation(): IContainer {
+        if (!this.#navigation) {
+            this.#navigation = new Container();
+            this.#navigation.backgroundColor = this.#colors.primary.COLOR_700;
+            this.#navigation.position = 'fixed';
+            this.#navigation.height = 64;
+            this.#navigation.left = 0;
+            this.#navigation.right = 0;
+            this.#navigation.top = 64;
+        }
+        return this.#navigation;
+    }
+
+    #topBar!: IContainer;
+
+    public get topBar(): IContainer {
+        if (!this.#topBar) {
+            this.#topBar = new Container();
+            this.#topBar.backgroundColor = this.#colors.primary.COLOR_900;
+            this.#topBar.position = 'fixed';
+            this.#topBar.height = 64;
+            this.#topBar.top = 0;
+            this.#topBar.left = 0;
+            this.#topBar.right = 0;
+        }
+        return this.#topBar;
     }
 }
 customElements.define('desktop-app', DesktopApp);
